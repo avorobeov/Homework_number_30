acaa6fb2f27356ab11f2d727353e1e0fbc54a9ef
воробйов александр <avorobeov21@gmail.com>
2023-05-04 16:30:07 +0300

    Добавьте файлы проекта.

---
+++ Homework_number_30/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_number_30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = GetNumber();
+
+            Console.WriteLine($"Вы получили число: {number}");
+            Console.ReadKey();
+        }
+
+        private static int GetNumber()
+        {
+            bool isNumber = false;
+            string userInput;
+            int number = 0;
+
+            while (isNumber == false)
+            {
+                Console.Write("Укажите число для конвертации: ");
+                userInput = Console.ReadLine();
+
+                if (int.TryParse(userInput, out number))
+                {
+                    isNumber = true;
+                }
+                else
+                {
+                    Console.WriteLine("К сожалению конвертация не удалась !");
+                }
+            }
+
+            return number;
+        }
+    }
+}
